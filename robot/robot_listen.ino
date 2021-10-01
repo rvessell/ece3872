250da03b5e8c1ed04d410f1fb6ff309fbc98b6ab
rvessell <71232141+rvessell@users.noreply.github.com>
2021-10-01 11:45:21 -0400

    Update robot_listen.ino

--- robot/robot_listen.ino
+++ robot/robot_listen.ino
@@ -42,14 +42,57 @@ void listen(){
       //bpm calculation from milliseconds
       double bpm = 60/(double)minMils * 1000 / 3;
       //enter our playback loop
-      doPlayback = true;
-      while (doPlayback)
-      {
-        doListen = false;
-        // this is where we call playback (maybe)
-        // something like playback(bpm);
-        // a press of our start button should kill this loop and the controlling loop
-      }
+      doListen = false;
+      playSong();
+      
     }
   }
+}
+
+void testListen()
+{
+  int count = 0;
+  int16_t* peakVals = new int16_t[4];
+  unsigned long* peakMils = new unsigned long[4];
+  bool attack = false;
+  int releaseCount = 0;
+  int attackCount = 0;
+  double peakVal;
+  
+  while(1) {
+  
+    peakVal = analogRead(0);
+    Serial.println(peakVal);
+    if (peakVal > 716 && !attack) {
+      if (attackCount < 20) {
+        attackCount++;
+      } else {
+        peakVals[count] = peakVal;
+        peakMils[count] = millis();
+        //Serial.println(peakMils[count]);
+        count++;
+        attack = true;
+        attackCount = 0;
+      }
+    } else if (peakVal <= 716) {
+      if (releaseCount < 20) {
+        releaseCount++;
+      } else {
+        attack = false;
+        releaseCount = 0;
+      }
+    }
+    // once we get enough samples, we measure for the shortest duration
+    if (count == 19) {
+      unsigned long minMils = 100000;
+      for (int i = 0; i < 4; ++i) {
+        if (peakMils[i + 1] - peakMils[i] < minMils) {
+          minMils = peakMils[i + 1] - peakMils[i];
+        }
+      }
+      //bpm calculation from milliseconds
+      double bpm = 60/(double)minMils * 1000;
+      //display on the screen the estimated BPM or blink an LED
+    }
+  }  
 }
